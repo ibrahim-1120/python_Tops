ddfa040022e80a1881041eaadb3d42a02a4ea46b
ibrahim <ibrahimburhanpurwala84@gmail.com>
2025-08-12 12:15:52 +0530

    add

---
+++ class work/Datatype.py
@@ -0,0 +1,25 @@
+a=10
+b=23.4
+c="defa"
+print(type(a))
+print(type(b))
+print(type(c))
+
+t =("a","b","c",1,2,3) 
+print(type(t))
+
+d = [1,2,3]
+print(type(d))
+
+bol=True
+
+print(type(bol))
+
+map={"name": "Alice", "age": 30}
+
+print(type(map))
+print(map)
+
+set={1,2,3}
+
+print(type(set))
