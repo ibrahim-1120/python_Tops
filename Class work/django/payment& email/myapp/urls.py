734fd29bfa5da7182b1aab12937f16593aea0b01
ibrahim <ibrahimburhanpurwala84@gmail.com>
2026-01-22 20:30:55 +0530

    add

--- Class work/django/payment& email/myapp/urls.py
+++ Class work/django/payment& email/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from myapp.views import *
 
 urlpatterns=[
-    path('',email,name='email')
+    path('',email,name='email'),
+    path('payment',payment,name='payment'),
+    path('pay',pay,name='pay')
 ]
--- Class work/django/payment& email/myapp/views.py
+++ Class work/django/payment& email/myapp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+import razorpay
 
 def email(request):
     return render(request,'email.html')
+
+def payment(request):
+    return render(request,'payment.html')
+
+def pay(request):
+    client = razorpay.Client(auth=("rzp_test_S1Hsg7YN8MlwDU", "ZKs1rK1XnjRDNd4uxjP2NcRJ"))
+
+    data = { "amount": 50000, "currency": "INR", "receipt": "order_rcptid_11" }
+    payment = client.order.create(data=data) # Amount is in currency subunits.
+
+    return HttpResponse(payment)
